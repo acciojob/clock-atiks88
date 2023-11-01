74baa67b2014b3fb5f722bc10ea0a4139e1f377b
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-11-01 12:27:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function updateDateTime() {
+  const dateTimeElement = document.getElementById("datetime");
+
+  // Create a Date object to get the current date and time
+  const currentDateTime = new Date();
+
+  // Update the content of the element with the current date and time
+  dateTimeElement.textContent = currentDateTime;
+}
+
+// Call the function to display the date and time initially
+updateDateTime();
+
+// Update the date and time every second (1000 milliseconds)
+setInterval(updateDateTime, 1000);
